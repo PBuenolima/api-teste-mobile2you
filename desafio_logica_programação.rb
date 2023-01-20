97ceba505fa434731643ce2bb095910fd755cce6
Pedro Bueno <pbuenolima@gmail.com>
2023-01-20 10:16:48 -0300

    Change variable name logic challenge

--- desafio_logica_programação.rb
+++ desafio_logica_programação.rb
@@ -1,4 +1,4 @@
 diamond_counter = 0
 expression = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>"
-expression.tr('.', '').tap { |expression_array| while expression_array.sub!(/<>/, ''); puts expression_array; diamond_counter += 1; end }
+expression.tr('.', '').tap { |expression_without_dot| while expression_without_dot.sub!(/<>/, ''); puts expression_without_dot; diamond_counter += 1; end }
 puts "Was found #{diamond_counter} diamonds"
